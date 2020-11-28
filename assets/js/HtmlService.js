4b01641e5dc45a202ae257ea63606ffcadc396b0
breno-do-val <brenorvale@gmail.com>
2020-11-28 15:33:32 -0300

    feat(crud): add list and create in html

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -2,17 +2,51 @@ export default class HtmlService {
   _todoService;
 
   constructor(todoService) {
-    this.bindFormEvent();
     this.todoService = todoService;
+    this.bindFormEvent();
+    this.listTasks();
   }
 
   bindFormEvent() {
     const form = document.querySelector("form");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
-      console.log("submitted! value: " + form.item.value);
+      this.addTask(form.item.value)
       form.reset();
       form.item.focus();
     });
+  }
+
+  async addTask(description) {
+    const task = { description, done: false };
+    const taskId = await this.todoService.save(task);
+    task.id = taskId;
+    this.addToHtmlList(task)
+  }
+
+  async listTasks() {
+    const tasks = await this.todoService.getAll();
+    tasks.forEach(task => {
+      this.addToHtmlList(task);
+    });
+  }
+
+  addToHtmlList(task) {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    span.textContent = task.description;
+    button.textContent = "x";
+
+    li.setAttribute("data-item-id", task.id);
+    if (task.done) {
+      li.classList.add("done");
+    }
+
+    li.appendChild(span);
+    li.appendChild(button);
+    ul.appendChild(li);
   }
 }
